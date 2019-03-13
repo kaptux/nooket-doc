e393cbdabafce33aee66a613e583059709adf2c1
Ramón Otero <kaptux@gmail.com>
2019-03-13 14:57:47 +0100

    Fix: humburger visibility

--- src/NooketDoc.tsx
+++ src/NooketDoc.tsx
@@ -22,12 +22,12 @@ const ListElementContainer = styled.div`
     display: inline-block;
     width: 11px;
     height: 11px;
-    background-image: url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50"><path d="M 0 7.5 L 0 12.5 L 50 12.5 L 50 7.5 L 0 7.5 z M 0 22.5 L 0 27.5 L 50 27.5 L 50 22.5 L 0 22.5 z M 0 37.5 L 0 42.5 L 50 42.5 L 50 37.5 L 0 37.5 z" color="#000"></path></svg>');
+    background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='50' height='50' viewBox='0 0 50 50' xml:space='preserve'%3E %3Cpath color='rgb(0, 0, 0)' d='M 0 7.5 L 0 12.5 L 50 12.5 L 50 7.5 L 0 7.5 z M 0 22.5 L 0 27.5 L 50 27.5 L 50 22.5 L 0 22.5 z M 0 37.5 L 0 42.5 L 50 42.5 L 50 37.5 L 0 37.5 z'/%3E %3C/svg%3E");
     background-size: contain;
     background-repeat: no-repeat;
     background-position: center;
     opacity: 0.25;
-    margin-right: 10px;
+    margin-right: 5px;
     cursor: row-resize;
   }
 
